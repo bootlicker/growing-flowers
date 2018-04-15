be15c4ad985b06175f673096e8814e7c62703169
Petri Autio <petri.mikael.autio@gmail.com>
2018-04-15 17:03:58 +0100

    Mesh height curves and LOD added

--- Growing Flower Game/Assets/Scripts/TerrainGen/MapGenerator.cs
+++ Growing Flower Game/Assets/Scripts/TerrainGen/MapGenerator.cs
@@ -11,8 +11,10 @@ public class MapGenerator : MonoBehaviour {
 
     public enum DrawMode {NoiseMap, ColourMap, Mesh}
     public DrawMode drawMode;
-    public int mapWidth;
-    public int mapHeight;
+
+    const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail; 
     public float noiseScale;
 
     public int octaves;
@@ -24,21 +26,21 @@ public class MapGenerator : MonoBehaviour {
     public Vector2 offset;
 
     public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
 
     public bool autoUpdate;
-
     public TerrainType[] regions;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
-        Color[] colourMap = new Color[mapWidth * mapHeight];
+        Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
 
         // Set colours by height on map
-        for (int y = 0; y < mapHeight; y++)
+        for (int y = 0; y < mapChunkSize; y++)
         {
-            for (int x = 0; x < mapWidth; x++)
+            for (int x = 0; x < mapChunkSize; x++)
             {
                 float currentHeight = noiseMap[x, y];
                 // Pick the right region to colour the garden by
@@ -46,7 +48,7 @@ public class MapGenerator : MonoBehaviour {
                 {
                     if (currentHeight <= regions[i].height)
                     {
-                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        colourMap[y * mapChunkSize + x] = regions[i].colour;
                         break;
                     }
                 }
@@ -61,23 +63,15 @@ public class MapGenerator : MonoBehaviour {
 
         } else if (drawMode == DrawMode.ColourMap)
         {
-            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
         } else if (drawMode == DrawMode.Mesh)
         {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
         }
     }
 
     private void OnValidate()
     {
-        if(mapWidth < 1)
-        {
-            mapWidth = 1;
-        }
-        if (mapHeight < 1)
-        {
-            mapHeight = 1;
-        }
         if (lacunarity < 1)
         {
             lacunarity = 1;
