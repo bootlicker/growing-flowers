66392cc234598bef383d840e4d62aa2b14edf23a
bootlicker <buhlairr@gmail.com>
2018-04-25 04:15:52 +1000

    camera and movement controls of model

--- Growing Flower Game/Assets/Player_Controller.cs
+++ Growing Flower Game/Assets/Player_Controller.cs
@@ -5,32 +5,40 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour {
 
     public float walkSpeed = 2;
-    
+    public float turnSmoothTime = 0.2f;
+    float turnSmoothVelocity;
+
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+    float currentSpeed;
+
     Animator animator;
+    Transform cameraT;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+        cameraT = Camera.main.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 			
 		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        
         Vector2 inputDir = input.normalized;
         
         if (inputDir != Vector2.zero) {
-        
-        transform.eulerAngles = Vector3.up * Mathf.Atan(inputDir.x/inputDir.y) * Mathf.Rad2Deg;
+            float targetRotation = Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
         }
         
-        float speed = (walkSpeed * inputDir.magnitude);
+        float targetSpeed = (walkSpeed * inputDir.magnitude);
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
         
-        transform.Translate (transform.forward * speed * Time.deltaTime, Space.World);
+        transform.Translate (transform.forward * currentSpeed * Time.deltaTime, Space.World);
         
         float animationSpeedPercent = (.5f * inputDir.magnitude);
         
-        animator.SetFloat("Speed Percent", animationSpeedPercent);
+        animator.SetFloat("Speed Percent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
 	}
 }
