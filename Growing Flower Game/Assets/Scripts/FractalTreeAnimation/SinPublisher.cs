d1edc77bc0ea252facb31f59e94fc2cdd12232b6
bootlicker <buhlairr@gmail.com>
2018-07-10 02:26:20 +1000

    fractal tree - this may break everything

---
+++ Growing Flower Game/Assets/Scripts/FractalTreeAnimation/SinPublisher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinPublisher : Publisher
+{
+
+    public float zeroValue = 1;
+    public float amplitude = 1;
+    public float timeScale = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PublishChange(zeroValue + Mathf.Sin(Time.time * timeScale) * amplitude);
+    }
+}
---
+++ Growing Flower Game/Assets/Scripts/FractalTreeGen/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    public float angleX = 30;
+    public float angleY = 90;
+    public float angleZ = 0;
+
+    public void Generated(RecursiveBundle bundle)
+    {
+        this.transform.rotation *= Quaternion.Euler(angleX * ((bundle.Index * 2) - 1), angleY * ((bundle.Index * 2) - 1), angleZ * ((bundle.Index * 2) - 1));
+    }
+}
